dec6043c87c5c4c8b26e0f071aad00df65c36e12
teddyzhu <teddyzhu15@gmail.com>
2015-12-21 18:57:14 +0800

    add font awesome and  icon less

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
-    autoprefixer = require('gulp-autoprefixer'),
+    autopreFixer = require('gulp-autoprefixer'),
     concat = require('gulp-concat'),
     rename = require('gulp-rename'),
-    minifycss = require('gulp-minify-css'),
+    minifyCss = require('gulp-minify-css'),
+    clean = require('gulp-clean'),
+    seQuence = require('run-sequence'),
+    excludeFolder = ['extra', 'mixins', 'variables'],
     exclude = ['reset', 'variables', 'header', 'util', 'normalize'],//不包含编译的文件
     config = {
         /* Minified CSS Concat */
@@ -21,9 +24,9 @@ var gulp = require('gulp'),
     };
 
 gulp.task('buildCSS', function () {
-    gulp.src(['src/less/*.less', '!src/less/{mixins,variables}/*.less', '!src/less/**/{' + exclude.join(',') + '}.less'])
+    return gulp.src(['./src/less/*.less', '!./src/less/{' + excludeFolder.join(',') + '}/*.less', '!./src/less/**/{' + exclude.join(',') + '}.less'])
         .pipe(less())
-        .pipe(autoprefixer({
+        .pipe(autopreFixer({
             browsers: ['last 2 versions', 'last 4 Explorer versions'],
             cascade: true, //是否美化属性值 默认：true
             remove: true //是否去掉不必要的前缀 默认：true
@@ -35,11 +38,28 @@ gulp.task('buildCSS', function () {
 });
 
 // 合并、压缩、重命名css
-gulp.task('minCSS', ['buildCSS'], function () {
-    gulp.src(['./dist/css/*.css'])
+gulp.task('minCSS', function () {
+    return gulp.src(['./dist/css/*.css'])
         .pipe(concat('sui.css')) // 合并文件
         .pipe(gulp.dest('./dist/')) // 输出
         .pipe(rename({suffix: '.min'})) // 重命名
-        .pipe(minifycss()) // 压缩css文件
+        .pipe(minifyCss()) // 压缩css文件
         .pipe(gulp.dest('./dist/')); // 输出
+});
+
+
+// 清空dist
+gulp.task('clean', function () {
+    return gulp.src(['./dist/'], {read: false})
+        .pipe(clean({force: true}));
+});
+
+// 将库文件对应到指定位置
+gulp.task('buildLib', function () {
+    return gulp.src(['./src/fonts/*'])
+        .pipe(gulp.dest('./dist/fonts/'));
+});
+
+gulp.task('deploy', function (done) {
+    seQuence('clean', 'buildCSS', 'minCSS', 'buildLib', done);
 });
